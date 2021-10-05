c37847b1e50a0738802ba4d2746f90973b28f615
Eduardo Cavalcante <epscavalcant@gmail.com>
2021-10-05 19:45:53 -0400

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Credentials invalid'], 400);
         }
 
         return $this->respondWithToken($token);
